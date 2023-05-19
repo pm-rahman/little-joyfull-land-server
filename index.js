434097758226c3de0f801132625b45d2ad9d18b2
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-05-19 09:56:43 +0600

    login user router create

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +11,6 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.maiu4ju.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,18 +27,32 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
 
-        const usersCollection = client.db('little-joyful-land').collection('users');
         const toyCollection = client.db('little-joyful-land').collection('toys');
 
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
-        app.post('/toys',async(req,res)=>{
+        app.get('/toys', async (req, res) => {
+            const result = await toyCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/user-toys', async (req, res) => {
+            const email = req.query.email;
+            const query = { email : req.query.email }
+            const result = await toyCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.findOne(query);
+            res.send(result);
+        })
+        app.post('/toy', async (req, res) => {
             const toy = req.body;
-            console.log(toy);
-            // const result = await toyCollection.insertOne(toy);
-            // res.send(result);
+            const result = await toyCollection.insertOne(toy);
+            res.send(result);
         })
 
 
